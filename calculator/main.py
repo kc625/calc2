ed983a0ba8c52a2c3df1776ebec0b2179da5d61a
kc625 <kc625@njit.edu>
2021-11-06 10:43:07 -0400

    Adjusted some parts of the main and test codes to make them cleaner and more uniform.

--- calculator/main.py
+++ calculator/main.py
@@ -1,12 +1,12 @@
-""" This is the increment function"""
+"""This class increments the result"""
 class Calculator:
-    """ This is the Calculator class"""
+    """ This is my Calculator class"""
 
     result = 0
     error = ""
 
     def get_result(self):
-        """ Get Result of Calculation"""
+        """Returns the result of a calculation"""
         return self.result
 
     def get_error(self):
@@ -14,13 +14,13 @@ class Calculator:
         return self.error
 
     def add_number(self, value_a):
-        """ adds number to result"""
-        self.result = self.result + value_a
+        """adds given number to result"""
+        self.result += value_a
         return self.result
 
     def subtract_number(self, value_a):
-        """ subtract number from result"""
-        self.result = self.result - value_a
+        """subtracts given number from result"""
+        self.result -= value_a
         return self.result
 
     def multiply_numbers(self, value_a, value_b):
@@ -31,7 +31,7 @@ class Calculator:
     def divide_numbers(self, value_a, value_b):
         """divides value_a by value_b, displays an error if value_b is 0"""
         if value_b == 0:
-            self.error = "Error, cannot divide by zero"
+            self.error = "Error, cannot divide by zero."
             return self.error
         self.result = value_a / value_b
         return self.result
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,46 +1,46 @@
-"""Testing the Calculator"""
+"""This tests the Calculator class"""
 from calculator.main import Calculator
 
-def test_calculator_result():
-    """testing calculator result is 0"""
+def test_calculator_initial_result():
+    """testing that Calculator result is initially 0"""
     calc = Calculator()
     assert calc.result == 0
 
-def test_calculator_add():
-    """Testing the Add function of the calculator"""
-    #Arrange by instantiating the calc class
-    calc = Calculator()
-    #Act by calling the method to be tested
-    calc.add_number(4)
-    #Assert that the results are correct
-    assert calc.result == 4
-
 def test_calculator_get_result():
-    """Testing the Get result method of the calculator"""
+    """Testing the get_result method of the calculator"""
     calc = Calculator()
     assert calc.get_result() == 0
+
+def test_calculator_add_number():
+    """Testing the add method of the calculator"""
+    #Arrange by instantiating the Calculator class as calc
+    calc = Calculator()
+    #Act by calling the method we are testing
+    calc.add_number(8)
+    #Assert that the results are correct and what we are expecting
+    assert calc.result == 8
 
 def test_calculator_subtract():
     """Testing the subtract method of the calculator"""
     calc = Calculator()
-    calc.subtract_number(1)
-    assert calc.get_result() == -1
+    calc.subtract_number(5)
+    assert calc.get_result() == -5
 
 def test_calculator_multiply():
     """Testing the multiply method of the calculator"""
     calc = Calculator()
-    calc.multiply_numbers(1,2)
-    assert calc.get_result() == 2
+    calc.multiply_numbers(4,5)
+    assert calc.get_result() == 20
 
 def test_calculator_divide():
     """Testing the divide method of the calculator"""
     calc = Calculator()
-    calc.divide_numbers(12,3)
-    assert calc.get_result() == 4
+    calc.divide_numbers(18,3)
+    assert calc.get_result() == 6
 
 def test_calculator_divide_by_zero():
-    """Tessting the divide method of the calculator
+    """Testing the divide method of the calculator
     when someone attempts to divide by zero"""
     calc = Calculator()
     calc.divide_numbers(4,0)
-    assert calc.get_error() == "Error, cannot divide by zero"
+    assert calc.get_error() == "Error, cannot divide by zero."
