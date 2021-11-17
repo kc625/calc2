7339721669421430887098564b84fee6161495c7
kc625 <kc625@njit.edu>
2021-11-16 22:33:43 -0500

    Working on Part 5, adjusted calculation file to use tuples and added "create" class method

--- calc/calculations/calculation.py
+++ calc/calculations/calculation.py
@@ -1,17 +1,21 @@
 """This is our base Calculation class"""
 class Calculation:
-    """Base Class used for the other calculations"""
+    """Abstract Base Class used for the other calculations"""
     # pylint: disable=too-few-public-methods
 
     def __init__(self, values: tuple):
         """Constructor method"""
-        self.values = Calculation.convert_args_to_list_float(values)
+        self.values = Calculation.convert_args_to_tuple_of_floats(values)
 
+    @classmethod
+    def create(cls, values: tuple):
+        """Factory method"""
+        return cls(values)
 
     @staticmethod
-    def convert_args_to_list_float(values):
-        """standardizes values to a list of floats"""
+    def convert_args_to_tuple_of_floats(values):
+        """standardizes values to a tuple of floats"""
         list_values_float = []
         for item in values:
             list_values_float.append(float(item))
-        return list_values_float
+        return tuple(list_values_float)
