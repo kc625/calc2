b9e401237e4690e287fa6e03ee417b1c8bb90ac5
kc625 <kc625@njit.edu>
2021-12-08 20:20:08 -0500

    Finished filling in code to make web-based calculator functional. Added logo I have been using for each project to insert instead of sample image the professor used.

--- app/app.py
+++ app/app.py
@@ -8,3 +8,36 @@ app = Flask(__name__)
 def index():
     """Index Route Response"""
     return render_template('index.html')
+
+@app.route('/basicform', methods=['GET', 'POST'])
+def basicform():
+    """Post Request Handling"""
+    if request.method == 'POST':
+        # Get the values out of the form
+        value1 = request.form['value1']
+        value2 = request.form['value2']
+        operation = request.form['operation']
+        # Make the Tuple
+        my_tuple = (value1, value2)
+        # This will call the correct operation
+        getattr(Calculator, operation)(my_tuple)
+        result = str(Calculator.get_last_result_value())
+        return render_template('result.html', value1=value1, value2=value2, operation=operation, result=result)
+    # Displays the form because if it isn't a post it is a get request
+    else:
+        return render_template('basicform.html')
+
+@app.route('/bad/<value1>/<value2>')
+def bad_calc(value1, value2):
+    """Bad Calculation Route Response"""
+    result = value1 + value2
+    response = 'The result of the calculation is: ' + result + '<a href="/"> back</a>'
+    return response
+
+@app.route('/good/<float:value1>/<float:value2>')
+def good_calc(value1, value2):
+    """Good Calculation Route Response"""
+    my_tuple = (value1, value2)
+    Calculator.addition(my_tuple)
+    response = 'The result of the calculation is: ' + str(Calculator.get_last_result_value()) + '<a href="/"> back</a>'
+    return response
